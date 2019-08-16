691243e15c467ba8e094166a396962106e03b136
Sanjay <sanjay.tamizharasu@gmail.com>
2019-08-16 14:38:41 -0700

    Initialized seasons project

---
+++ seasons/src/index.js
@@ -0,0 +1,14 @@
+//Seasons Application
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+  return (<div>Hi there!</div>);
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"))
