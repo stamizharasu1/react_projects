b8ba414ef6d1f3bdb2c20e938c91d21e9795792b
Sanjay Tamizharasu <sanjay.tamizharasu@gmail.com>
2019-08-19 21:16:34 -0700

    Finished Seasons project. Learned about React state and lifecycle methods.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,34 +2,36 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null, errorMessage: ''};
-
+  componentDidMount() {
+    //Inherit setState from React.Component -- use it to set the state
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //Inherit setState from React.Component -- use it to set the state
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  //React needs us to define the render method
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat= {this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please Accept Our Location Request!"/>;
+  }
+
+  //React needs us to define the render method
+  render() {
+    return (
+      <div>{this.renderContent()}</div>
+    );
   }
 }
 
