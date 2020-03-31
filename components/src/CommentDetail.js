dd291300e880f4184e2c0d52181f6f45d4bcfa4f
Sanjay Tamizharasu <sanjay.tamizharasu@gmail.com>
2020-03-31 10:32:21 -0700

    Beginning forms lesson

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,55 +1,24 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-            <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-            <a href="/" className="author">
-                Sam
-            </a>
-            <div className="metadata">
-                <span className="data">Today at 6:00pm</span>
-            </div>
-                <div className="text"> Nice work. </div>
-            </div>
-        </div>
-
-        <div className="comment">
-            <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-            <a href="/" className="author">
-                Amira
-            </a>
-            <div className="metadata">
-                <span className="data">Today at 6:00pm</span>
-            </div>
-                <div className="text"> Good stuff! </div>
-            </div>
-        </div>
-
-        <div className="comment">
-            <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-            <a href="/" className="author">
-                Damian
-            </a>
-            <div className="metadata">
-                <span className="data">Today at 6:00pm</span>
-            </div>
-                <div className="text"> SPY puts? How original. </div>
-            </div>
+const CommentDetail = (props) => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src= {props.profpic} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {props.author}
+          </a>
+          <div className="metadata">
+              <span className="data"> {props.date} </span>
+          </div>
+            <div className="text"> {props.content}. </div>
+          </div>
         </div>
     </div>
-    );
+  );
 };
 
 export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container elements">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <br></br>
+      <br></br>
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          <p>Are you sure you want to show this?</p>
+        </div>
+        <CommentDetail
+          author="Sam"
+          date="Today at 4:45pm"
+          profpic ={faker.image.avatar()}
+          content = "Nice!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Tom"
+          date="Today at 2am"
+          profpic ={faker.image.avatar()}
+          content= "How are you feeling?"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          date="Yesterday at 5pm"
+          profpic ={faker.image.avatar()}
+          content="Good stuff mate."
+        />
+      </ApprovalCard>
     </div>
   );
 };
