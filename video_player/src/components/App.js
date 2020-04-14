53b57d83fd9386c1f15bd810504ee5cda7c361ec
Sanjay Tamizharasu <sanjay.tamizharasu@gmail.com>
2020-04-14 15:00:01 -0700

    added components for videolist and videodetail view. showing videos now in iframes.

--- video_player/src/components/App.js
+++ video_player/src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube'
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const KEY = 'AIzaSyC0vSVDETRlLzYSld_lGeqhuYANkYpBAiQ';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term,
         part: 'snippet',
@@ -14,12 +18,23 @@ class App extends React.Component {
         key: KEY
       }
     });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos} 
+        />
       </div>
     );
   }
